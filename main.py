a2ab0b8b456e919e8283dcbbcbf8ab9b39189300
Alexey Zaytsev <ai.zaytsev89@gmail.com>
2024-08-31 12:41:08 -0300

    fixed

--- main.py
+++ main.py
@@ -1,97 +1,114 @@
-import requests
+import aiohttp
+import asyncio
+import os
 from sys import argv
+import ssl
+import certifi
 
-def get_compfund_see_odo_sum_from_page(url, page_num):
-    r = requests.post(url, json={"filters": {"member_status": 1}, "page": page_num, "pageCount": "100", "sortBy": {}})
-    data_dict = r.json()
+async def fetch(session, url, json_data=None):
+    async with session.post(url, json=json_data) as response:
+        return await response.json()
+
+async def get_compfund_see_odo_sum_from_page(session, url, page_num):
+    json_data = {"filters": {"member_status": 1}, "page": page_num, "pageCount": "100", "sortBy": {}}
+    data_dict = await fetch(session, url, json_data)
     data = data_dict['data']['data']
-    compensation_fund_fee_odo_per_page = 0
-    for i in data:
-        try:
-            compensation_fund_fee_odo_per_page = int(i['compensation_fund_fee_odo']) + compensation_fund_fee_odo_per_page
-        except:
-            pass
+    compensation_fund_fee_odo_per_page = sum(float(i.get('compensation_fund_fee_odo', 0)) for i in data)
     return compensation_fund_fee_odo_per_page
 
-def get_number_of_pages(url):
-    r = requests.post(url, json={"filters": {"member_status": 1}, "page": 1, "pageCount": "100", "sortBy": {}})
-    data_dict = r.json()
+async def get_number_of_pages(session, url):
+    json_data = {"filters": {"member_status": 1}, "page": 1, "pageCount": "100", "sortBy": {}}
+    data_dict = await fetch(session, url, json_data)
     number_of_pages = int(data_dict['data']['countPages'])
     return number_of_pages
 
-def get_compfund_see_odo_sum_per_sro(url):
+async def get_compfund_see_odo_sum_per_sro(session, url):
     comfund_odo_total = 0
-    for page in range(1, get_number_of_pages(url)+1):
-        comfund_odo_total = comfund_odo_total + get_compfund_see_odo_sum_from_page(url, page)
+    number_of_pages = await get_number_of_pages(session, url)
+    tasks = [get_compfund_see_odo_sum_from_page(session, url, page) for page in range(1, number_of_pages + 1)]
+    results = await asyncio.gather(*tasks)
+    comfund_odo_total = sum(results)
     return comfund_odo_total
 
 def write_to_file(filename, data):
     with open(filename, "a") as file:
         file.write(data)
 
-def get_nostroy_dict_items():
-    i = 266
+async def get_nostroy_dict_items(session):
     nostroy_sro_dict = {}
-    while i < 600:
-        sro_url = "https://api-open-nostroy.anonamis.ru/api/sro/" + str(i)
-        r = requests.post(sro_url)
-        if r.status_code == 200:
-            data_dict = r.json()
+    for i in range(1, 600):
+        sro_url = f"https://reestr.nostroy.ru/api/sro/{i}"
+        data_dict = await fetch(session, sro_url)
+        print(data_dict)
+        try:
+            data_dict = await fetch(session, sro_url)
             short_description = data_dict['data']['short_description']
             registration_number = data_dict['data']['registration_number']
-            dict_key = registration_number + ' ' + short_description
+            dict_key = f"{registration_number} {short_description}"
             nostroy_sro_dict[dict_key] = i
-            print("Added " + short_description + " with ID " + str(i) )
-        else:
-            print(str(i) + " not found")
-        i = i + 1
+            print(f"Added {short_description} with ID {i}")
+        except:
+            print(f"{i} not found")
     return nostroy_sro_dict.items()
 
-def get_nopriz_dict_items():
-    i = 0
+async def get_nopriz_dict_items(session):
     nopriz_sro_dict = {}
-    while i < 600:
-        sro_url = "https://reestr.nopriz.ru/api/sro/" + str(i)
-        r = requests.post(sro_url)
-        if r.status_code == 200:
-            data_dict = r.json()
+    for i in range(1, 600):
+        sro_url = f"https://reestr.nopriz.ru/api/sro/{i}"
+        try:
+            data_dict = await fetch(session, sro_url)
             short_description = data_dict['data']['short_description']
             registration_number = data_dict['data']['registration_number']
-            dict_key = registration_number + ' ' + short_description
+            dict_key = f"{registration_number} {short_description}"
             nopriz_sro_dict[dict_key] = i
-            print("Added " + short_description + " with ID " + str(i) )
-        else:
-            print(str(i) + " not found")
-        i = i + 1
+            print(f"Added {short_description} with ID {i}")
+        except:
+            print(f"{i} not found")
     return nopriz_sro_dict.items()
 
-def main():
+async def main():
     script, sro = argv
-    
-    if sro == 'nostroy':
-        filename = "/home/alex/Work/nostroy.txt"
-        nostroy_dict_items = get_nostroy_dict_items()
-        print("Dict full")
-        for key, value in nostroy_dict_items:
-            url = 'https://api-open-nostroy.anonamis.ru/api/sro/' + str(value) +'/member/list'
-            data = key + " : " + str(get_compfund_see_odo_sum_per_sro(url)) + "\n"
-            print("Starting writing to file")
-            write_to_file(filename, data)
-            print("Info added to file")
-        print("Script comleted with status: Done")
-    elif sro == 'nopriz':
-        filename = "/home/alex/Work/nopriz.txt"
-        nopriz_dict_items = get_nopriz_dict_items()
-        print("Dict full")
-        for key, value in nopriz_dict_items:
-            url = 'https://reestr.nopriz.ru/api/sro/' + str(value) +'/member/list'
-            data = key + " : " + str(get_compfund_see_odo_sum_per_sro(url)) + "\n"
-            print("Starting writing to file")
-            write_to_file(filename, data)
-            print("Info added to file")
-        print("Script comleted with status: Done")
-    else:
-        print('Unknown argument: '+ sro + '. Please type "nopriz" or "nostroy"')
-    
+
+    home_dir = os.path.expanduser("~")
+    ssl_context = ssl.create_default_context(cafile=certifi.where())
+
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=ssl_context)) as session:
+        if sro == 'nostroy':
+            filename = os.path.join(home_dir, "nostroy.txt")
+            nostroy_dict_items = await get_nostroy_dict_items(session)
+            print("Dict full")
+            tasks = []
+            for key, value in nostroy_dict_items:
+                url = f"https://api-open-nostroy.anonamis.ru/api/sro/{value}/member/list"
+                task = asyncio.create_task(get_compfund_see_odo_sum_per_sro(session, url))
+                tasks.append((key, task))
+
+            for key, task in tasks:
+                data = f"{key} : {await task}\n"
+                print("Starting writing to file")
+                write_to_file(filename, data)
+                print("Info added to file")
+            print("Script completed with status: Done")
+
+        elif sro == 'nopriz':
+            filename = os.path.join(home_dir, "nopriz.txt")
+            nopriz_dict_items = await get_nopriz_dict_items(session)
+            print("Dict full")
+            tasks = []
+            for key, value in nopriz_dict_items:
+                url = f"https://reestr.nopriz.ru/api/sro/{value}/member/list"
+                task = asyncio.create_task(get_compfund_see_odo_sum_per_sro(session, url))
+                tasks.append((key, task))
+
+            for key, task in tasks:
+                data = f"{key} : {await task}\n"
+                print("Starting writing to file")
+                write_to_file(filename, data)
+                print("Info added to file")
+            print("Script completed with status: Done")
+
+        else:
+            print(f'Unknown argument: {sro}. Please type "nopriz" or "nostroy"')
+
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
